7f1e3be7d04e51adc0af7311ec0a8a0a00efe305
PulakBala <pulakbala0524@gmail.com>
2022-10-06 23:56:39 +0600

    recap-storage

---
+++ recap-storage/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input id='product' type="text" placeholder='product' />
+      <input id='quantity' type="text" placeholder='quantity' />
+      <button>Add</button>
+      <hr />
+      <h3>Added Products</h3>
+
+    </div>
+  );
+}
+
+export default App;
