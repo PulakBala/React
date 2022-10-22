43d9a73773a9cb6a52d4dada1c9c6b16525137a3
PulakBala <pulakbala0524@gmail.com>
2022-10-22 18:04:46 +0600

    tawilwind hero icon add

--- price-club/src/App.js
+++ price-club/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Nav from './Components/Navbar/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
       <h1 className='text-3xl font-bold '>This is a Tailwind</h1>
       <p>This is a paragraph</p>
     </div>
