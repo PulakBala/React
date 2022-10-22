45aa4d163715dc3fa1ddb54ec22f135dc1883896
PulakBala <pulakbala0524@gmail.com>
2022-10-22 16:55:31 +0600

    react with tailwind

---
+++ price-club/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl font-bold '>This is a Tailwind</h1>
+      <p>This is a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
