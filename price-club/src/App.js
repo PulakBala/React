143ea91b673ecada1a63e68351c9d40867d0ff00
PulakBala <pulakbala0524@gmail.com>
2022-10-24 19:47:24 +0600

    add phoneBar

--- price-club/src/App.js
+++ price-club/src/App.js
@@ -2,16 +2,17 @@
 import './App.css';
 import AssignmentMarks from './Components/AssignmentMarks/AssignmentMarks';
 import Nav from './Components/Navbar/Nav';
+import PhoneBar from './Components/PhoneBar/PhoneBar';
 import Pricing from './Components/Pricing/Pricing';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
-      <h1 className='text-3xl font-bold '>This is a Tailwind</h1>
-      <p>This is a paragraph</p>
       <Pricing></Pricing>
       <AssignmentMarks></AssignmentMarks>
+      <PhoneBar></PhoneBar>
+      
     </div>
   );
 }
---
+++ price-club/src/Components/PhoneBar/PhoneBar.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+const PhoneBar = () => {
+       const [phones, setPhones] = useState([]);
+
+       useEffect(()=>{
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+        .then(data=> {
+            const phonesLoaded = data.data.data;
+            const phoneData = phonesLoaded.map(phone => {
+                const parts = phone.slug.split('-');
+                const price = parseInt(parts[1]);
+                const singlePhone = {
+                    name: phone.phone_name,
+                    price: price
+                }
+                return singlePhone;
+            })
+            console.log(phoneData);
+            setPhones(phoneData);
+        });
+       },[])
+    return (
+        <div>
+            <BarChart width={500} height={200} data={phones}>
+          <Bar dataKey="price" fill="#8884d8" />
+          <XAxis/>
+          <YAxis/>
+          <Tooltip></Tooltip>
+        </BarChart>
+        </div>
+    );
+};
+
+export default PhoneBar;
