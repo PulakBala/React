41cfc065b463ea4f21dc7ce91bd7fcec9ba04907
PulakBala <pulakbala0524@gmail.com>
2022-10-30 11:26:01 +0600

    routers-link add

--- explore-routes/src/App.js
+++ explore-routes/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   const router = createBrowserRouter([
       {path: '/', element: <div>This is Defalut page</div>},
       {path: '/home', element: <Home></Home>},
-      {path: 'about', element: <About></About>}
+      {path: 'about', element: <About></About>},
+      {path: 'contact', element: <Contact></Contact>}
   ]);
   return (
     <div className="App">
--- explore-routes/src/Components/Header/Header.js
+++ explore-routes/src/Components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../css/header.css'
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <Link to='/home'>Home</Link>
             <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
         </div>
     );
 };
