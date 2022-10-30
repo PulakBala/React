dab689b69e35f4ff9fe10210bc18ac761d2160cc
PulakBala <pulakbala0524@gmail.com>
2022-10-30 23:32:45 +0600

    post user data daynamic

--- explore-routes/src/Components/Friends/Friends.js
+++ explore-routes/src/Components/Friends/Friends.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Friend from '../Friend/Friend';
-
+import './Friends.css'
 const Friends = () => {
     const friends = useLoaderData();
     return (
-        <div>
+    
+        <div >
+            <div>
             <h2>My Friends : {friends.length}</h2>
-            {
+                
+           </div>       
+           <div className='friends'>
+           {
                 friends.map(friend => <Friend
                     key={friend.id}
                     friend={friend}
                 ></Friend>)
             }
+           </div>
         </div>
     );
 };
--- explore-routes/src/Components/PostsDetails/PostsDetails.js
+++ explore-routes/src/Components/PostsDetails/PostsDetails.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './PostsDetails.css'
 const PostsDetails = ({post}) => {
-    const {title, body} = post;
+    const {id, title, body} = post;
     return (
         <div className='post'>
             <h2>A lot of facebook posts here.</h2>
             <h3>Headline : {title}</h3>
             <h4>Summery : {body}</h4>
+            
+            <Link to={`/post/${id}`}>
+              <button>Show Details</button>
+            </Link>
         </div>
     );
 };
